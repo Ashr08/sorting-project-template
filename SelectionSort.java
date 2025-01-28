dec20566f87b6ecb7fbc030f7c1a4ab518fd47da
Ashr08 <ashrithgandluru@gmail.com>
2025-01-28 07:09:10 +0000

    SelectionSort Method

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,6 +1,24 @@
 public class SelectionSort implements Sorter {
 
-    public void sort(int[] input) {
-        System.out.println("Selection Sort!!!");
-    }
+
+   public void sort(int[] input) {
+       System.out.println("Selection Sort!!!");
+
+
+       int n = input.length;
+       for (int i = 0; i < n - 1; i++) {
+
+
+           for (int j = i + 1; j < n; j++) {
+               if (input[j] < input[minIndex]) {
+                   minIndex = j;
+               }
+           }
+
+
+           int temp = input[minIndex];
+           input[minIndex] = input[i];
+           input[i] = temp;
+       }
+   }
 }
